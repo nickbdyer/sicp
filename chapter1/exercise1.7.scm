884a21fad07beade5a3dae66184481acb22cd109
Nick Dyer <nickbdyer@users.noreply.github.com>
2017-05-29 16:07:51 +0100

    Add exercise 1.7, new sqrt procedure

--- chapter1/exercise1.7.scm
+++ chapter1/exercise1.7.scm
@@ -3,3 +3,21 @@
 
 (good-enough? 0.1 0.01)
 (good-enough? 5000 2345098273458)
+
+(define (square x)
+  (* x x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (new-good-enough? guess improved-guess)
+  (< (abs (- guess improved-guess)) (* guess 0.001)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (sqrt-iter guess x)
+  (if (new-good-enough? guess (improve guess x))
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
