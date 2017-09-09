af6a684819dc72fba4c17f1498060199996d674e
Nick Dyer <nickbdyer@users.noreply.github.com>
2017-09-09 13:28:33 +0100

    Add exercise 2.21

---
+++ chapter2/exercise2.21.scm
@@ -0,0 +1,10 @@
+(define (square-list items)
+  (if (null? items)
+    '()
+    (cons (square (car items)) (square-list (cdr items)))))
+
+(define (square-list-map items)
+  (map (lambda (x) (square x)) items))
+
+(square-list (list 1 2 3 4))
+(square-list-map (list 1 2 3 4))
