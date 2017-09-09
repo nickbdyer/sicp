e9ced3b21703fab7c898bf0980d9bec6d33a8d6d
Nick Dyer <nickbdyer@users.noreply.github.com>
2017-09-09 13:18:57 +0100

    Update exercise 2.20

--- chapter2/exercise2.20.scm
+++ chapter2/exercise2.20.scm
@@ -1,11 +1,11 @@
 (define (same-parity first . rest)
   (if (even? first)
-    (create-new-list even? rest first)
-    (create-new-list odd? rest first)))
+    (create-new-list even? rest (list first))
+    (create-new-list odd? rest (list first))))
 
 (define (create-new-list pred old-list new-list)
   (cond ((null? old-list) new-list)
-        ((pred (car old-list)) (create-new-list pred (cdr old-list) (cons new-list (car old-list))))
+        ((pred (car old-list)) (create-new-list pred (cdr old-list) (append new-list (list (car old-list)))))
         (else (create-new-list pred (cdr old-list) new-list))))
 
 (same-parity 1 2 3 4 5 6 7)
